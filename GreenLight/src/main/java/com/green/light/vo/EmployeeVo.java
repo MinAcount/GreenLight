723e91f2dc7712ac3b2424ff5c0aa2705f2df1c1
xoals4050 <xoals4050@gmail.com>
2024-03-12 10:28:10 +0900

    0312 김태민 아키텍쳐 패키지 추가 mainSidenav.jsp 파일 서명 링크 수정

---
+++ GreenLight/src/main/java/com/green/light/vo/EmployeeVo.java
@@ -0,0 +1,38 @@
+package com.green.light.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeVo {
+
+	private int id;
+	private String name;
+	private String password;
+	private String email;
+	private String phone;
+	private String birthday;
+	private String gender;
+	private String address;
+	private String deptno;
+	private String spot;
+	private String position;
+	private String estatus;
+	private String join_day;
+	private String exit_day;
+	private String etype;
+	private int leave;
+	private String auth;
+	private String profile;
+	private int fail;
+
+}
