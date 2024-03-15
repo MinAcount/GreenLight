fccec623027380be0710d7a0805f2457d5e50b0f
xoals4050 <xoals4050@gmail.com>
2024-03-15 17:03:41 +0900

    0315 금 김태민 Attendaince Vacation Signature 아키텍쳐 일부 추가 및 시그니처 junit test 완료

---
+++ GreenLight/src/test/java/com/test/ktm/AttendanceJUnitTest.java
@@ -0,0 +1,14 @@
+package com.test.ktm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AttendanceJUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
