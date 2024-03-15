a9fb65b0cb88573ab5786a03fe4a0b8478f1b4a0
limstar113 <limstar113@naver.com>
2024-03-15 17:25:36 +0900

    5:25 만수무강

--- GreenLight/src/main/java/com/green/light/vo/SchedulesVo.java
+++ GreenLight/src/main/java/com/green/light/vo/SchedulesVo.java
@@ -2,4 +2,23 @@ package com.green.light.vo;
 
 public class SchedulesVo {
 
+	private String schedule_id;
+	private String cno;
+	private String creator;
+	private String phone;
+	private String modified;
+	private String usertype;
+	private String category;
+	private String title;
+	private String memo;
+	private String start_date;
+	private String end_date;
+	private String location;
+	private String priority;
+	private String recur;
+	private String visibility;
+	private String participants;
+	private String permission;
+	private String alarm;
+
 }
