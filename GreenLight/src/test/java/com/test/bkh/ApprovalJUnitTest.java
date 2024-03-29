7c32ebf1ffafed968e6695295ca30307946f71fa
smilelris <smilelris@naver.com>
2024-03-29 09:02:40 +0900

    0329 금요일 이혜원 결재선관련 수정, 기안서 관련 수정 jstreejs 수정

--- GreenLight/src/test/java/com/test/bkh/ApprovalJUnitTest.java
+++ GreenLight/src/test/java/com/test/bkh/ApprovalJUnitTest.java
@@ -9,16 +9,25 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.green.light.model.service.IApprovalService;
+import com.green.light.model.service.IDepartmentService;
 import com.green.light.vo.ApprJstreeVo;
+import com.green.light.vo.ApprovalVo;
+import com.green.light.vo.DepartmentVo;
 import com.green.light.vo.EmployeeVo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+@WebAppConfiguration
 public class ApprovalJUnitTest {
 	@Autowired
 	private IApprovalService service;
+	
+	@Autowired
+	private IDepartmentService deptService;
+	
 
 	@Test
 	public void test() {
@@ -28,10 +37,10 @@ public class ApprovalJUnitTest {
 //		assertNotNull(vo1);
 		
 		// 부서별 부서장 조회 02
-		EmployeeVo vo2 = service.selectDeptMgrByDept("09");
-		System.out.println("======================================================= "+vo2);
-		assertNotNull(vo2);
-	
+//		DepartmentVo vo2 = deptService.selectDeptMgrByDept("09");
+//		System.out.println("======================================================= "+vo2);
+//		assertNotNull(vo2);
+//	
 //	// 대표이사 조회
 //		EmployeeVo vo3 = service.selectCEO();
 //		System.out.println(vo3);
@@ -41,6 +50,17 @@ public class ApprovalJUnitTest {
 //		List<ApprJstreeVo> list = service.getAllApprJstreeView();
 //		System.out.println("-------------------------------------------------------"+list);
 //		assertNotEquals(0, list.size());
+		
+		
+		// 결재자 추가
+		
+		ApprovalVo vo = new ApprovalVo();
+		vo.setWriter_id("2403110902");
+		vo.setEmp_id("2401110301");
+		vo.setOrderno(3);
+		int n = service.insertApproval(vo);
+		assertEquals(1, n);
+
 	}
 
 }
