ed702d476367fb6f5a0b76efef7baff1d74bd0d8
xoals4050 <xoals4050@gmail.com>
2024-04-04 09:03:31 +0900

    0404 목 김태민 연차자 근태 관련 cron 설정

--- GreenLight/src/main/java/com/green/light/model/mapper/CronDaoImpl.java
+++ GreenLight/src/main/java/com/green/light/model/mapper/CronDaoImpl.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import com.green.light.vo.AttendanceVo;
 import com.green.light.vo.VacationVo;
 
 import lombok.extern.slf4j.Slf4j;
@@ -43,5 +44,41 @@ public class CronDaoImpl implements ICronDao{
 		log.info("CronDaoImpl insertOverVacation 연차초과사용분 조정 크론");
 		return session.insert(NS+"insertOverVacation", map);
 	}
+	
+	@Override
+	public int updateLeaveNewEmployee() {
+		log.info("CronDaoImpl updateLeaveNewEmployee 1년 미만 직원 연차 부여 크론");
+		return session.update(NS+"updateLeaveNewEmployee");
+	}
 
+	@Override
+	public int updateLeaveEmployee() {
+		log.info("CronDaoImpl updateLeaveEmployee 1년 이상 근속직원 연차부여 크론");
+		return session.update(NS+"updateLeaveEmployee");
+	}
+
+	@Override
+	public List<VacationVo> getEmpVacationStatus() {
+		log.info("CronDaoImpl getEmpVacationStatus 작일 직원 휴가 상태 조회");
+		return session.selectList(NS+"getEmpVacationStatus");
+	}
+
+	@Override
+	public AttendanceVo getEmpAttendanceStatus(String id) {
+		log.info("CronDaoImpl getEmpAttendanceStatus 작일 직원 휴가 상태 조회");
+		return session.selectOne(NS+"getEmpAttendanceStatus", id);
+	}
+
+	@Override
+	public int insertVacationAttendance(AttendanceVo vo) {
+		log.info("CronDaoImpl insertVacationAttendance 휴가직원 근태 자동등록");
+		return session.insert(NS+"insertVacationAttendance", vo);
+	}
+
+	@Override
+	public int updateVacationAttendance(AttendanceVo vo) {
+		log.info("CronDaoImpl updateVacationAttendance 휴가직원 근태 자동변경");
+		return session.update(NS+"updateVacationAttendance", vo);
+	}
+	
 }
--- GreenLight/src/main/java/com/green/light/model/service/ICronService.java
+++ GreenLight/src/main/java/com/green/light/model/service/ICronService.java
@@ -3,6 +3,7 @@ package com.green.light.model.service;
 import java.util.List;
 import java.util.Map;
 
+import com.green.light.vo.AttendanceVo;
 import com.green.light.vo.VacationVo;
 
 public interface ICronService {
@@ -12,5 +13,25 @@ public interface ICronService {
 	public int insertOverVacation(Map<String, Object> map);
 	
 	public int OverVacationMediate();
+	
+	public int updateLeaveNewEmployee();
+	
+	public int updateLeaveEmployee();
+	
+//	작일 직원휴가상태조회	getEmpVacationStatus
+	public List<VacationVo> getEmpVacationStatus();
+	
+//	작일 출근현황조회	getEmpAttendanceStatus
+	public AttendanceVo getEmpAttendanceStatus(String id);
+	
+//	작일 휴가자 근태등록	insertVacationAttendance
+	public int insertVacationAttendance(AttendanceVo vo);
+	
+//	작일 휴가자 근태수정	updateVacationAttendance
+	public int updateVacationAttendance(AttendanceVo vo);
+	
+//	휴가자 자동 크론설정
+	public int setVacationAndAttendance();
+	
 
 }
