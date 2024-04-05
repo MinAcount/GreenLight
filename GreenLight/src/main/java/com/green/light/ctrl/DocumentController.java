8f4a64cfbf2963db30cfd089cf6ec87ba0164b92
easy <2jiwons2@gmail.com>
2024-04-05 14:52:25 +0900

    240405 14:52 (금) 이지원 중간긴급푸쉬

--- GreenLight/src/main/java/com/green/light/ctrl/DocumentController.java
+++ GreenLight/src/main/java/com/green/light/ctrl/DocumentController.java
@@ -294,7 +294,7 @@ public class DocumentController {
 		service.updateDocStatus(docMap);
 		writerId.add(writer_id);
 		if(appr_status.equals("03") && orderno == lists.size() || appr_status.equals("03") && orderno != lists.size()) {
-			notiMap.put("content", "[" + title + "] 문서가 반려("+map.get("comment")+")되었습니다.");
+			notiMap.put("content", "[" + title + "] 문서가 반려(반려사유:"+map.get("comment")+")되었습니다.");
 		}else if(!appr_status.equals("03") && orderno == lists.size()){
 			notiMap.put("content", "[" + title + "] 문서가 승인되었습니다.");
 		}
